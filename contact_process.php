28e2fc546de1b24a91cc7c44c74b41908ee4a6d2
Weyler Jorge <weylerjorge5@gmail.com>
2023-10-25 19:16:05 -0300

    contact process

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,33 @@
 <?php
-
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
-
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    $subject = "You have a message from your Bitmap Photography.";
-
-    $logo = 'img/logo.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "weylerjorge5@gmail.com"; // Substitua pelo seu endereço de e-mail
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $name = $_POST["name"];
+    $from = $_POST["email"];
+    
+    $headers = "From: $from\r\n";
+    $headers .= "Reply-To: $from\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    
+    $message_body = "
+    <html>
+    <body>
+    <h2>Contato de $name</h2>
+    <p><strong>Assunto:</strong> $subject</p>
+    <p><strong>Mensagem:</strong></p>
+    <p>$message</p>
+    </body>
+    </html>
+    ";
+    
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem. Por favor, tente novamente mais tarde.";
+    }
+} else {
+    echo "Acesso não autorizado.";
+}
 ?>
