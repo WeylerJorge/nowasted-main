6aadd2f0a9164d2dd883b844164e6454374d6cce
Weyler Jorge <weylerjorge5@gmail.com>
2023-10-26 15:17:24 -0300

    legendas_materiais_aceitos

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -1,3 +1,18 @@
+class TipoItem {
+  constructor(nome, icone) {
+    this.nome = nome;
+    this.icone = icone;
+  }
+}
+
+const tiposDeItens = [
+  new TipoItem("Eletrodomésticos", "eletrodomesticos.png"),
+  new TipoItem("Bateria", "bateria.png"),
+  new TipoItem("Celular", "celular.png"),
+  new TipoItem("Placa para Computador", "placa_computador.png"),
+  new TipoItem("HD", "hd.png"),
+];
+
 const pontosDeColeta = [
   {
     lat: -19.9255805,
@@ -5,6 +20,7 @@ const pontosDeColeta = [
     nome: "EcoPonto Fonte Grande",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[1]], // Exemplo: Aceita Eletrodomésticos e Baterias
   },
   {
     lat: -19.9570892,
@@ -12,6 +28,7 @@ const pontosDeColeta = [
     nome: "BH Recicla Reciclagem de Lixo Eletrônico e Outras Sucatas.",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[3], tiposDeItens[4]], // Exemplo: Aceita Placas para Computador e HD
   },
   {
     lat: -20.00549,
@@ -19,6 +36,7 @@ const pontosDeColeta = [
     nome: "Emile Reciclagem de Lixo Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Eletrodomésticos, Celulares e HD
   },
   {
     lat: -19.9431076,
@@ -26,13 +44,15 @@ const pontosDeColeta = [
     nome: "Reciclagem Horizonte BH",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[1], tiposDeItens[2], tiposDeItens[3]], // Exemplo: Aceita Baterias, Celulares e Placas para Computador
   },
   {
     lat: -19.9453905,
     lng: -43.9224724,
-    nome: "E-MILE Reciclagem de Lixo Eletrônico - Agende a coleta por whats app 31 99544-9937",
+    nome: "E-MILE Reciclagem de Lixo Eletrônico - Agende a coleta por WhatsApp 31 99544-9937",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Celulares e HD
   },
   {
     lat: -19.9408104,
@@ -40,6 +60,7 @@ const pontosDeColeta = [
     nome: "MG Recicla Reciclagem de Lixo Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[3]], // Exemplo: Aceita Placas para Computador
   },
   {
     lat: -19.8919873,
@@ -47,6 +68,7 @@ const pontosDeColeta = [
     nome: "SOS GREEN Gestão de Descarte Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[1]], // Exemplo: Aceita Eletrodomésticos e Baterias
   },
   {
     lat: -19.9139503,
@@ -54,6 +76,7 @@ const pontosDeColeta = [
     nome: "Lithium Informática - Ecoponto E-MILE - Descarte Lixo Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Celulares e HD
   },
   {
     lat: -19.8120751,
@@ -61,6 +84,7 @@ const pontosDeColeta = [
     nome: "Inovar Ambiental Coleta, Transporte e Tratamento de Resíduos",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[3]], // Exemplo: Aceita Eletrodomésticos e Placas para Computador
   },
   {
     lat: -19.8766497,
@@ -68,6 +92,7 @@ const pontosDeColeta = [
     nome: "Ponto de Coleta de Lixo Eletrônico - PROPAM",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[1]], // Exemplo: Aceita Baterias
   },
   {
     lat: -19.853842,
@@ -75,6 +100,7 @@ const pontosDeColeta = [
     nome: "Reciclar Minas",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[4]], // Exemplo: Aceita HD
   },
   {
     lat: -19.9542588,
@@ -82,6 +108,7 @@ const pontosDeColeta = [
     nome: "Impulso Reciclar",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[1]], // Exemplo: Aceita Eletrodomésticos e Baterias
   },
   {
     lat: -19.9151839,
@@ -89,6 +116,7 @@ const pontosDeColeta = [
     nome: "Projeto Pro Verde - Lixo Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[3]], // Exemplo: Aceita Celulares e Placas para Computador
   },
   {
     lat: -19.8496913,
@@ -96,6 +124,7 @@ const pontosDeColeta = [
     nome: "Emile Coleta de Lixo Eletronico MG",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Celulares e HD
   },
   {
     lat: -19.925871,
@@ -103,6 +132,7 @@ const pontosDeColeta = [
     nome: "Ecoponto E-MILE de Lixo Eletrônico (CTRL+Play)",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[3]], // Exemplo: Aceita Eletrodomésticos e Placas para Computador
   },
   {
     lat: -19.9102212,
@@ -110,6 +140,7 @@ const pontosDeColeta = [
     nome: "E-MILE Coleta Lixo Eletrônico",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Celulares e HD
   },
   {
     lat: -19.7861035,
@@ -117,6 +148,7 @@ const pontosDeColeta = [
     nome: "Reciclagem da Preta",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[1]], // Exemplo: Aceita Eletrodomésticos e Baterias
   },
   {
     lat: -19.9297365,
@@ -124,6 +156,7 @@ const pontosDeColeta = [
     nome: "WHARGO Comércio e Reciclagem",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[3], tiposDeItens[4]], // Exemplo: Aceita Placas para Computador e HD
   },
   {
     lat: -19.9528975,
@@ -131,6 +164,7 @@ const pontosDeColeta = [
     nome: "Trindade Reciclagem",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[2], tiposDeItens[4]], // Exemplo: Aceita Celulares e HD
   },
   {
     lat: -19.95776069999999,
@@ -138,6 +172,7 @@ const pontosDeColeta = [
     nome: "Reciclagem Tc",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[1], tiposDeItens[3]], // Exemplo: Aceita Baterias e Placas para Computador
   },
   {
     lat: -19.9288201,
@@ -145,6 +180,7 @@ const pontosDeColeta = [
     nome: "Reciclagem Santa Maria",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[2]], // Exemplo: Aceita Eletrodomésticos e Celulares
   },
   {
     lat: -19.9180323,
@@ -152,6 +188,7 @@ const pontosDeColeta = [
     nome: "CMR - Reciclagem de Resíduos e Sucatas Ltda",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[1], tiposDeItens[3], tiposDeItens[4]], // Exemplo: Aceita Baterias, Placas para Computador e HD
   },
   {
     lat: -19.9130812,
@@ -159,6 +196,7 @@ const pontosDeColeta = [
     nome: "Reciclagem Lunardi",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[2]], // Exemplo: Aceita Eletrodomésticos e Celulares
   },
   {
     lat: -19.9099723,
@@ -166,6 +204,7 @@ const pontosDeColeta = [
     nome: "Reciclagem Lagoinha",
     descricao: "",
     avaliacao: 0,
+    itensAceitos: [tiposDeItens[3], tiposDeItens[4]], // Exemplo: Aceita Placas para Computador e HD
   },
   {
     lat: -19.9176766,
@@ -173,17 +212,11 @@ const pontosDeColeta = [
     nome: "Reciclagem Campo alto",
     descricao: "",
     avaliacao: 0,
-  },
-  {
-    lat: -19.9768006,
-    lng: -44.0258627,
-    nome: "PUC Minas Barreiro",
-    descricao: "",
-    avaliacao: 0,
+    itensAceitos: [tiposDeItens[0], tiposDeItens[1], tiposDeItens[2], tiposDeItens[3], tiposDeItens[4]], // Aceita todos os tipos de itens
   },
 ];
-  
-		function inicializarMapa() {
+
+function inicializarMapa() {
   const localizacaoInicial = { lat: -19.917887, lng: -43.937270 };
   const mapa = new google.maps.Map(document.getElementById("mapa"), {
     zoom: 11,
@@ -197,6 +230,7 @@ const pontosDeColeta = [
       position: { lat: ponto.lat, lng: ponto.lng },
       map: mapa,
       title: ponto.nome,
+      icon: './assets/img/elements/map_icon.png', // Substitua pelo caminho correto do ícone
     });
 
     const infoWindow = new google.maps.InfoWindow({
@@ -204,6 +238,14 @@ const pontosDeColeta = [
         <strong>${ponto.nome}</strong>
         <p>${ponto.descricao}</p>
         <p>Avaliação: ${ponto.avaliacao}</p>
+        <p>Itens Aceitos:</p>
+        <ul style="list-style: none; padding: 0;">
+          ${ponto.itensAceitos.map((tipoItem) => `
+            <li style="margin: 5px 0;">
+              <img src="./assets/img/icon/${tipoItem.icone}" alt="${tipoItem.nome}" width="30" height="30" />
+              ${tipoItem.nome}
+            </li>`).join('')}
+        </ul>
       `,
     });
 
@@ -220,4 +262,3 @@ const pontosDeColeta = [
     infoWindows.push(infoWindow);
   });
 }
-	 
